c35e57263fe19a104cce50e294f0e6869a0bc3f1
Yaroslav Sudnik <yaroslav.sudnik@flatstack.com>
2017-07-13 13:26:45 +0300

    merge to master

--- app/src/free/java/com/jrummy/busybox/installer/MyFirebaseMessagingService.java
+++ app/src/free/java/com/jrummy/busybox/installer/MyFirebaseMessagingService.java
@@ -10,6 +10,7 @@ import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.jrummy.busybox.installer.activities.MainActivity;
 import com.jrummyapps.busybox.R;
 
 import java.util.Map;
